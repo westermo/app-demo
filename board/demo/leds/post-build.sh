9fdc0b62a05a75a18701b6e1b560c37e86078b8b
Andreas Egeberg <andreas.egeberg@westermo.se>
2021-05-31 13:24:26 +0200

    Pull Req #2: Correcting name of application, and expanding description.
    Signed-off-by: Andreas Egeberg <andreas.egeberg@westermo.com>

--- board/demo/leds/post-build.sh
+++ board/demo/leds/post-build.sh
@@ -6,9 +6,9 @@ TARGET_DIR=$1
 # original Buildroot information.
 rm $TARGET_DIR/etc/os-release
 
-echo "NAME=Leds"                                   >$TARGET_DIR/etc/os-release
+echo "NAME=LED Demo"                              >$TARGET_DIR/etc/os-release
 echo "VERSION=1.0.0"                              >>$TARGET_DIR/etc/os-release
-echo "ID=leds"                                    >>$TARGET_DIR/etc/os-release
+echo "ID=led"                                     >>$TARGET_DIR/etc/os-release
 echo "VERSION_ID=1.0.0"                           >>$TARGET_DIR/etc/os-release
-echo "PRETTY_NAME=\"Leds Demo\""                  >>$TARGET_DIR/etc/os-release
+echo "PRETTY_NAME=\"LED Demo\""                   >>$TARGET_DIR/etc/os-release
 
