240107eef2f5ed529cd9dcd3be2d7429374c2c5b
Joachim Wiberg <troglobit@gmail.com>
2021-07-09 14:48:28 +0200

    Call common/post-image.sh at to rename rootfs.squash to proper name
     - Rename all rootfs.squash to their proper name based on the demo and
       platform.  E.g. rootfs.squash -> dhcp-boot-coronet.img
     - Prepare for versioned image names, when building releases, using
       the same setup as in NetBox; the RELEASE variable must be set to
       the GIT tag name
     - This commit also creates a .sha1 checksum file that can be used for
       integrity checking.  Not to be confused with the hash.sha1 that is
       the GitHub internal hash for build jobs
     - Also, drop any intermediate rootfs.tar that may have been created
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ board/common/post-image.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+. $BR2_CONFIG 2>/dev/null
+
+# Figure out version suffix for image files, default to empty suffix for
+# developer builds.  After a long discussion, this turned out to be the
+# least contentious alternative to: -dev, -devel, -HEAD, etc.
+err=0
+ver=""
+if [ -n "$RELEASE" ]; then
+    # NOTE: Must use `-f $BR2_EXTERNAL` here to get, e.g. app-demo GIT version
+    ver="-$($BR2_EXTERNAL_NETBOX_PATH/bin/mkversion -f $BR2_EXTERNAL)"
+
+    if [ "$RELEASE" != "$ver" ]; then
+       echo "==============================================================================="
+       echo "WARNING: Release verision '$RELEASE' does not match tag '$ver'!"
+       echo "==============================================================================="
+       err=1
+    fi
+fi
+
+img="${BINARIES_DIR}/${BR2_TARGET_GENERIC_HOSTNAME}-${APP_PLATFORM}${ver}.img"
+md5=$(basename "$img" .img).md5
+
+mv "$BINARIES_DIR"/rootfs.sqash "$img"
+sha1sum "$img" > "$md5"
+
+# Clean up intermediate files
+rm -f $BINARIES_DIR/rootfs.tar
+
+exit $err
