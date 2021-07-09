5465239bc438bb6540ff3393e1f7450775fdc8f5
Joachim Wiberg <troglobit@gmail.com>
2021-07-09 17:54:25 +0200

    configs: update DHCP-Boot defconfigs
    Also, drop demo specific generation of /etc/os-release, this is now done
    in the common post-build script.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- board/demo/dhcp-boot/post-build.sh
+++ board/demo/dhcp-boot/post-build.sh
@@ -2,20 +2,9 @@
 
 TARGET_DIR=$1
 
-# This is a symlink to /usr/lib/os-release, so we remove this to keep
-# original Buildroot information.
-rm $TARGET_DIR/etc/os-release
-
-echo "NAME=dhcp-boot"                               >$TARGET_DIR/etc/os-release
-echo "VERSION=1.0.0"                              >>$TARGET_DIR/etc/os-release
-echo "ID=dhcp-boot"                                >>$TARGET_DIR/etc/os-release
-echo "VERSION_ID=1.0.0"                           >>$TARGET_DIR/etc/os-release
-echo "PRETTY_NAME=\"Dhcp-boot Demo\""              >>$TARGET_DIR/etc/os-release
-
 # Add start of dhcp-boot to /etc/inittab if it doesn't already exist
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    if ! grep -q "::sysinit:/usr/sbin/create-config.sh" ${TARGET_DIR}/etc/inittab;
-    then
+    if ! grep -q "::sysinit:/usr/sbin/create-config.sh" ${TARGET_DIR}/etc/inittab; then
         sed -i '/now run any rc scripts/i\::sysinit:/usr/sbin/create-config.sh' ${TARGET_DIR}/etc/inittab;
     fi
 fi
